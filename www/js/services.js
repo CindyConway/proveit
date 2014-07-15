310c5537a16d34efc453f3cb06a97e282f31362d
CindyConway <cindy@cindyconwaysair.att.net>
2014-07-14 18:57:00 -0700

    added_friend

--- www/js/services.js
+++ www/js/services.js
@@ -11,7 +11,8 @@ angular.module('starter.services', [])
     { id: 0, name: 'Scruff McGruff' },
     { id: 1, name: 'G.I. Joe' },
     { id: 2, name: 'Miss Frizzle' },
-    { id: 3, name: 'Ash Ketchum' }
+    { id: 3, name: 'Ash Ketchum' },
+    { id: 4, name: 'Darleen Larin' }
   ];
 
   return {
